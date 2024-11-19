ffd8c6c12e3578b88b1ab465b60bc120085a62fa
Yan Qiao Yu <yqy1160058763@qq.com>
2024-11-19 18:31:52 +0800

    添加ddddocr识别

--- app/api/v2/captchaRecog.py
+++ app/api/v2/captchaRecog.py
@@ -1,10 +1,9 @@
-import requests
-from fastapi import APIRouter, File, Form, UploadFile
+from fastapi import APIRouter, File, Form,  UploadFile
 from pydantic import BaseModel
 from utils.recognize import Recognize
 import time
 import os
-
+import re
 captchaRecogRouter = APIRouter()
 
 # 存放验证码的路径
@@ -73,6 +72,7 @@ async def PostRecogReq(
 
     # 这里只读6M,一来是为了能够触发下面的比较,二来是不想读太多影响效率
     picture_contents = await picture.read(1024*1024*6)
+
     if len(picture_contents) > allowed_picture_size:
         result_model = ResponseModel(
             data={"status_code": 400},
@@ -83,6 +83,7 @@ async def PostRecogReq(
         return result_model
 
     # 3.保存验证码图片
+
     # 毫秒级的时间戳,避免出现图片名重复导致异常
     picture_name = picture.filename.split(
         '.')[0] + str(round(time.time() * 1000)) + '.png'
@@ -139,114 +140,105 @@ async def PostRecogReq(
                   }
         )
 
-@captchaRecogRouter.post("/api/v2/recognize_session", response_model=ResponseModel)
-async def PostRecogReq(
-        product_name: str = Form(..., max_length=96, description="产品线名称"),
-        # picture: UploadFile = File(..., description="验证码图片"),
-        captchahost: str = Form(..., max_length=96, description="被测服务Host")
+
+def ocr(img) -> str:
+    """
+    通过带带弟弟库识别图片中的文本
+    @param    img: 图片
+    @return:  字符串
+    """
+    import ddddocr
+    dddd = ddddocr.DdddOcr(show_ad=False)
+    return dddd.classification(img)
+
+
+def strip_verify_code(code: str) -> str:
+    return re.sub(r'\W+', '', code)
+
+
+@captchaRecogRouter.post("/api/v2/apex/recognize", response_model=ResponseModel)
+async def PostApexRecogReq(picture: UploadFile = File(..., description="验证码图片")
 ):
     '''
-        处理图像识别的接口，并返回Session
-        '''
+    处理图像识别的接口，使用ddddocr库
+    '''
 
-    train_data = ''
-    picture_name = ''
-    recognize_result = ''
-    session = ''
-
-    # 1. 确定产品线及其对应的训练数据
-    if product_name not in product_map:
-        allowed_product_name_list = []
-        for key in product_map:
-            allowed_product_name_list.append(key)
-
+    # 1.确定验证码图片类型及图片大小
+    if picture.filename.split('.')[-1] not in allowed_picture_type_list:
         result_model = ResponseModel(
             data={"status_code": 400},
             meta={
-                "message": f"当前暂不支持{product_name}产品,支持的产品如下",
-                "supported_product": allowed_product_name_list
+                "message": f"当前暂不支持{picture.filename.split('.')[-1]}格式,支持的文件类型如下",
+                "supported_type": allowed_picture_type_list
             }
         )
         return result_model
-    else:
-        train_data = product_map[product_name]
 
-    # 通过captchaurl下载对应图片,并保存，
-    if captchahost:
-        # 获取验证码图片地址
-        captchaurl = r"https://%s/dashboard/auth/captcha" % captchahost
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-        payload=""
-        try:
-            res = requests.get(str(captchaurl), headers=headers, data=payload, verify=False,stream=True)
-            session = "session="+res.cookies.get("session")
+    # 这里只读6M,一来是为了能够触发下面的比较,二来是不想读太多影响效率
+    picture_contents = await picture.read(1024*1024*6)
 
-            # 毫秒级的时间戳,避免出现图片名重复导致异常
-            picture_name = 'captcha'+str(round(time.time() * 1000)) + '.png'
-            with open(os.path.join(save_picture_path, picture_name), 'wb') as f:
-                for chunk in res.iter_content(chunk_size=128):
-                    f.write(chunk)
-                f.close()
+    if len(picture_contents) > allowed_picture_size:
+        result_model = ResponseModel(
+            data={"status_code": 400},
+            meta={
+                "message": f"验证码过大,已经超过5MB",
+            }
+        )
+        return result_model
 
-        except Exception as e:
-            result_model = ResponseModel(
-                data={"status_code": 500,
-                      },
-                meta={"message": "验证码图片处理失败",
-                      "error": e
-                      }
-            )
-            return result_model
+    # 2.保存验证码图片
 
-        try:
-            # 4.进行图像识别, 得到结果
-            recognize_result = Recognize(picture_name)
-        except Exception as e:
-            # 5.删除保存的图片
-            os.remove(save_picture_path + picture_name)
-            result_model = ResponseModel(
-                data={"status_code": 500,
-                      },
-                meta={"message": "验证码图片识别失败",
-                      "error": e
-                      }
-            )
-            return result_model
+    # 毫秒级的时间戳,避免出现图片名重复导致异常
+    picture_name = picture.filename.split(
+        '.')[0] + str(round(time.time() * 1000)) + '.png'
 
-        # 5.删除保存的图片
-        os.remove(save_picture_path + picture_name)
-
-        # 6.返回结果
-        if recognize_result:
-            result_model = ResponseModel(
-                data={"status_code": 200,
-                      "result": recognize_result,
-                      "cookie": session
-                      },
-                meta={"message": "识别成功",
-                      "used_train_data": train_data
-                      }
-            )
-
-            return result_model
-        else:
-            result_model = ResponseModel(
-                data={"status_code": 500,
-                      "result": recognize_result,
-                      "cookie": session
-                      },
-                meta={"message": "识别失败",
-                      "used_train_data": train_data
-                      }
-            )
-            return result_model
-    else:
+    try:
+        with open(f"{save_picture_path}{picture_name}", 'wb') as f:
+            f.write(picture_contents)
+        f.close()
+    except Exception as e:
         result_model = ResponseModel(
             data={"status_code": 500,
                   },
-            meta={"error_message": "被测服务Host不存在，请检查BODY参数captchahost"
+            meta={"message": "验证码图片处理失败",
+                  "error": e
                   }
         )
         return result_model
+
+
+
+
+    # 3. 使用ddddocr进行图像识别
+    try:
+        recognize_result = ocr(picture_contents)
+    except Exception as e:
+        result_model = ResponseModel(
+            data={"status_code": 500},
+            meta={"message": "验证码图片识别失败",
+                  "error": str(e)
+                  }
+        )
+        return result_model
+
+    # 4. 返回结果
+    if recognize_result:
+        result_model = ResponseModel(
+            data={"status_code": 200,
+                  "result": strip_verify_code(recognize_result)
+                  },
+            meta={"message": "识别成功",
+                  "used_train_data": None
+                  }
+        )
+    else:
+        result_model = ResponseModel(
+            data={"status_code": 500,
+                  "result": recognize_result
+                  },
+            meta={"message": "识别失败",
+                  "used_train_data": None
+                  }
+        )
+
+    return result_model
